29d419583ddc9aef68404ce9bb74d4c45cdecff3
Kimberly Brisby <cappykim7@yahoo.com>
2026-09-20 10:49:54 -0500

    restarted and broke everything done to re code and see where i missed up at

--- latest/src/main.jsx
+++ latest/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+
+import `./styles/main.scss`;
+
+const root = document.getElementById(`root`);
+
+const NEW_COOLDOWN_HS = 5800;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App cooldown={NEW_COOLDOWN_HS} />
   </StrictMode>,
 )
