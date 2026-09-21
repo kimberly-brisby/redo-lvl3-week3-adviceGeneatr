11f58de277b8020c312ea0d07be98cf8face2d72
Kimberly Brisby <cappykim7@yahoo.com>
2026-09-21 16:39:04 -0500

    i got the code to work properly but the color is off

--- latest/src/components/useAdvice.js
+++ latest/src/components/useAdvice.js
@@ -1,11 +1,21 @@
-const adviceId = document.querySelector(`#advice`);
-const adviceText = document.querySelector(`adviceText`);
-const btn = document.querySelector(`#btn`);
+const adviceId = document.querySelector(".advice span");
+const adviceText = document.querySelector(".advicetext p");
+const adviceBtn = document.querySelector(".btn");
 
-function getAdvice(){
-    fetch("https://api.adviceslip.com/advice").then(response =>){
-        return response.json();
-    }
-   
-
+const fetchData = async () => {
+    try {
+    const res = await fetch("https://api.adviceslip.com{Date.now()}");
+    const data = await res.json()
+    const {slip} = data
+    adviceId.innerHTML = `#${slip.id}`
+    adviceText.innerHTML = `${slip.advice}`
+}catch (error){
+    console.error("Error fetching advice:",error);
 }
+
+
+};
+
+fetchData();
+adviceBtn.addEventListener("click", fetchData);
+
